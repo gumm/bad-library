31e1618b07796c99de102a4534e46aafdf3028ad
gumm <janhendrik.badenhorst@gmail.com>
2014-06-25 23:12:23 +0200

    MQTT Parse Update. Removed app logic from parser

--- bad/data/mqttparse.js
+++ bad/data/mqttparse.js
@@ -4,11 +4,8 @@
  */
 goog.provide('bad.MqttParse');
 goog.provide('bad.MqttParse.replyCode');
-goog.provide('bad.TCTEvent');
-goog.provide('bad.TCTReceivedEvent');
 
 goog.require('goog.array');
-goog.require('goog.events.Event');
 goog.require('goog.events.EventTarget');
 goog.require('goog.json');
 goog.require('goog.object');
@@ -20,52 +17,8 @@ goog.require('goog.object');
  */
 bad.MqttParse = function() {
   goog.events.EventTarget.call(this);
-
-  /**
-   * @type {Object.<string, Function>} A map of command names
-   *    to their parsing functions. Use {@code addCommandParseFunc} to add to
-   *    this map.
-   * @private
-   */
-  this.commandParseFuncs_ = {};
-
-  /**
-   * @type {Object.<string, Function>} A map of event codes and their
-   *    corresponding parsing functions. All event names are the string
-   *    equivalents of event numbers. Use {@code addEventParseFunc} to add to
-   *    this map.
-   * @private
-   */
-  this.eventParseFuncs_ = {};
-
-  /**
-   * This is the default parsing function for parsing data messages.
-   * It can be set with {@code setDataParseFunc}
-   * @type {Function}
-   * @private
-   */
-  this.dataParseFunc_ = goog.nullFunction;
-
-  /**
-   * The default parsing function to parse null payloads with.
-   * It can be set with {@code setNullFunc}
-   * @type {Function}
-   * @private
-   */
-  this.nullFunc_ = goog.nullFunction;
 };
 goog.inherits(bad.MqttParse, goog.events.EventTarget);
-
-/**
- * @typedef {{code: !number, tct: !string, tcr: !string,
- *    ts: !Date, msg: !Array, res: *}}
- */
-bad.MqttParse.NormData;
-
-/**
- * @typedef {{x: !Array}}
- */
-bad.MqttParse.XMsg;
 
 /**
  * Enum for reply codes.
@@ -75,99 +28,135 @@ bad.MqttParse.XMsg;
  * @enum {number}
  */
 bad.MqttParse.replyCode = {
-  // Could only parse some of the given data.
-  PARTIAL_DATA_SUCCESS: 2,
 
-  // Could only parse some of the given events.
-  PARTIAL_EVENT_SUCCESS: 1,
+  /**
+   * Could only parse some of the given data.
+   */
+  BAD_TIMESTAMP: 1,
 
-  // Command or event received and parsed.
+  /**
+   * Command or event received and parsed.
+   */
   ALL_OK: 0,
 
-  // Could not resolve the given command keyword to a known command.
-  UNKNOWN_COMMAND: -1,
+  /**
+   * Raised when the given payload is the empty array
+   */
+  PAYLOAD_EMPTY: -1,
 
-  // Could not resolve the given event code to a known event code.
-  UNKNOWN_ERROR_CODE: -2,
+  /**
+   *  The given payload is not an array.
+   */
+  PAYLOAD_NOT_ARRAY: -2,
 
-  // No data parser available.
-  DATA_NOT_PARSED: -3
+  /**
+   *  The given payload does not contain at least 2 elements.
+   */
+  PAYLOAD_TOO_SHORT: -3,
+
+  /**
+   *  The given payload does contains more than 3 or 4(for x types) elements.
+   */
+  PAYLOAD_TOO_LONG: -4,
+
+  /**
+   * The message is not of the type c, d, e, x or n.
+   */
+  TYPE_ERR: -5,
+
+  /**
+   * The command type not an array.
+   */
+  COMMAND_NOT_ARRAY: -6,
+
+  /**
+   * Raised when the command array is empty.
+   */
+  COMMAND_ARR_EMPTY: -7,
+
+  /**
+   * Raised when the first element of the command array is not a string.
+   */
+  COMMAND_STRING: -8,
+
+  /**
+   * The event type not an array.
+   */
+  EVENTS_NOT_ARRAY: -9,
+
+  /**
+   * Raised when the event array is empty.
+   */
+  EVENTS_ARR_EMPTY: -10,
+
+  /**
+   * The event type not an array.
+   */
+  EVENT_NOT_ARRAY: -11,
+
+  /**
+   * Raised when the event array is empty.
+   */
+  EVENT_ARR_EMPTY: -12,
+
+  /**
+   * Raised when the event code is not a number.
+   */
+  EVENT_CODE_NOT_NUMBER: -13
+
+
 };
+
+/**
+ * This parser internally normalises the MQTT payload to a data structure
+ * that is passed into its internal and proxy parsing functions.
+ * @typedef {{code: !number, tct: !string, tcr: !string,
+ *    ts: !Date, msg: !Array, res: *}}
+ */
+bad.MqttParse.NormData;
 
 /**
  * @param {!string} payload A JSON parsable string.
  */
 bad.MqttParse.prototype.parse = function(payload) {
   var obj = goog.json.parse(payload);
-  var map = {
-    'c': this.parseCommand_,
-    'd': this.parseData_,
-    'e': this.parseEvent_,
-    'n': this.nullFunc_,
-    'x': this.parseXReply_
-  };
-
-  goog.object.forEach(map, function(func, type) {
-    if (obj[type]) {
-      /**
-       * @type {!bad.MqttParse.NormData}
-       */
-      var normPayload = this.normalizePayload_(type, obj[type]);
-      console.debug(type, 'Normalised Payload ----> ', normPayload);
-      this.parseWith_(normPayload, func);
-    }
-  }, this);
+  var type = goog.object.getAnyKey(obj);
+  var normPayload = null;
+  if (type) {
+    /**
+     * @type {!bad.MqttParse.NormData}
+     */
+    normPayload = this.normalizePayload_(type, obj[type]);
+  }
+  return normPayload;
 };
 
-/**
- * @param {!string} type
- * @param {!Array} val
- * @return {!bad.MqttParse.NormData}
- * @private
- */
-bad.MqttParse.prototype.normalizePayload_ = function(type, val) {
-  var reply = {};
-  if (
-      goog.isArray(val) &&                        // Is array, with
-      val.length >= 2 &&                          // min 2 el, and
-      (type !== 'x' ? val.length <= 3 : true) &&  // 3 el max if type != "x", or
-      (type === 'x' ? val.length <= 4 : true) &&  // 4 el max if type == "x",
-      goog.isNumber(val[0])                       // and 1st element is a number
-      ) {
-    reply.ts = this.parseTimeStamp_(val[0]);
+//----------------------------------------------------------[ Normalise Data ]--
 
-    var second = val[1];
-    var typeOfSecond = goog.typeOf(second);
-    var third = val[2];
+bad.MqttParse.prototype.testPayloadType_ = function(type, val, reply) {
+  var passIsArray = this.isArray(val);
+  var passNotEmpty = !this.isEmptyArr(val);
+  var passIsKnownType = goog.string.contains('cdex', type);
 
-    switch (type) {
-      case 'c':
-      case 'd':
-      case 'e':
-        if (typeOfSecond === 'array') {
-          reply.msg = /** @type {!Array} */ (second);
-        } else if (typeOfSecond === 'string') {
-          reply.tct = /** @type {!string} */ (second);
-          reply.msg = /** @type {!Array} */ (third);
-        }
-        break;
-      case 'n':
-        break;
-      case 'x':
-        if (typeOfSecond === 'string') {
-          reply.tcr = /** @type {string} */ (second);
-        }
-        if (goog.isDef(third) && goog.isNumber(third)) {
-          reply.code = /** @type {number} */ (third);
-        }
-        if (val.length === 4) {
-          reply.res = val[3];
-        }
-        break;
-      default:
-        console.debug('Unknown type:', type);
-    }
-  }
+  reply.code =
+    !passIsArray ? bad.MqttParse.replyCode.PAYLOAD_NOT_ARRAY :
+    !passNotEmpty ? bad.MqttParse.replyCode.PAYLOAD_EMPTY :
+    !passIsKnownType ? bad.MqttParse.replyCode.TYPE_ERR :
+    bad.MqttParse.replyCode.ALL_OK;
+  return reply;
+};
+
+bad.MqttParse.prototype.testPayloadLength_ = function(type, val, reply) {
+  var len = val.length;
+  var passMin = type === 'n' ? len === 1 : len >= 2;
+  var passMax = type === 'n' ? len === 1 :
+                type === 'x' ? len <= 4:
+                len <= 3;
+
+  reply.code =
+    !passMin ? bad.MqttParse.replyCode.PAYLOAD_TOO_SHORT :
+    !passMax ? bad.MqttParse.replyCode.PAYLOAD_TOO_LONG :
+    reply.code;
   return reply;
 };
 
@@ -181,220 +170,198 @@ bad.MqttParse.prototype.normalizePayload_ = function(type, val) {
  * @return {!Date}
  * @private
  */
-bad.MqttParse.prototype.parseTimeStamp_ = function(ts) {
-  var reply = new Date();
-  if (ts < 0) {
-    reply.setSeconds(reply.getSeconds() + ts);
-  } else if (ts > 0) {
-    var milli = ts * 1000;
-    var now = reply.valueOf();
-    reply = now > milli ? reply.setTime(milli) : reply;
+bad.MqttParse.prototype.parseTimeStamp_ = function(ts, reply) {
+  reply.code = bad.MqttParse.replyCode.BAD_TIMESTAMP;
+  var date = new Date();
+  if (this.isNumber(ts)) {
+    reply.code = bad.MqttParse.replyCode.ALL_OK;
+    if (ts < 0) {
+      date.setSeconds(date.getSeconds() + ts);
+    } else if (ts > 0) {
+      var milli = ts * 1000;
+      date.setTime(milli);
+    }
+    reply.ts = date;
   }
   return reply;
 };
 
-/**
- * @param {!bad.MqttParse.NormData} norm The normalised payload data.
- * @param {!Function} func The function that will be used to parse the data.
- * @private
- */
-bad.MqttParse.prototype.parseWith_ = function(norm, func) {
-  if (norm.tct) {
-    var now = new Date();
-
-    /**
-     * @type {bad.MqttParse.XMsg}
-     */
-    var xReply = {'x': []};
-    var xArr = [now.valueOf(), norm.tct];
-
-    goog.array.extend(xArr, func.call(this, norm));
-    xReply.x = xArr;
-    this.dispatchEvent(new bad.TCTEvent(this, norm.tct, xReply));
-  } else {
-    if (func) {
-      func.call(this, norm);
-    }
+bad.MqttParse.prototype.parseHeader_ = function(type, val, reply) {
+  reply = this.parseTimeStamp_(val[0], reply);
+  if (!reply.code) {
+    var first = val[1];
+    var second = val[2];
+    var third = val[3];
+    switch(type + val.length) {
+      case 'x3':
+        reply.tcr = first;
+        reply.code = second;
+        break;
+      case 'x4':
+        reply.tcr = first;
+        reply.code = second;
+        reply.res = third;
+        break;
+      case 'c2':
+        reply = this.parseCommandMsg_(first, reply);
+        break;
+      case 'c3':
+        reply.tct = first;
+        reply = this.parseCommandMsg_(second, reply);
+        break;
+      case 'd2':
+        reply.data = first;
+        break;
+      case 'd3':
+        reply.tct = first;
+        reply.data = second;
+        break;
+      case 'e2':
+        reply = this.parseEventMsg_(first, reply);
+        break;
+      case 'e3':
+        reply.tct = first;
+        reply = this.parseEventMsg_(second, reply);
+        break;
+      default:
+          console.debug('We really should not have come here', type, val.length);
+      }
   }
+  return reply;
+
 };
 
-/**
- * @param {(Function|null|undefined)} fn The proxy parsing function.
- * @param {Array} data The normalised data.
- * @param {!number} failCode The failure code. Used when the given fn is
- *    not a function.
- * @return {!Array}
- * @private
- */
-bad.MqttParse.prototype.doIt_ = function(fn, data, failCode) {
-  var arr = [];
-  if (goog.isFunction(fn)) {
-    arr.push(bad.MqttParse.replyCode.ALL_OK);
-    var result = fn.apply(this, data);
-    if (goog.isDef(result)) {
-      arr.push(result);
-    }
-  } else {
-    arr.push(failCode);
-  }
-  return arr;
-};
+bad.MqttParse.prototype.parseEventMsg_ = function(msg, reply) {
 
-//-----------------------------------------------------------------[ Command ]--
+  reply.code =
+    !this.isArray(msg) ? bad.MqttParse.replyCode.EVENTS_NOT_ARRAY :
+    this.isEmptyArr(msg) ? bad.MqttParse.replyCode.EVENTS_ARR_EMPTY :
+    reply.code;
+  var broken = {};
+  if (!reply.code) {
 
-/**
- * Parses the normalised data according to the function name contained in the
- * msg component of the data. A lookup is done into the available command parse
- * functions mapped in commandParseFuncs_ map.
- * @param {!bad.MqttParse.NormData} norm The normalised data structure
- * @return {!Array} The reply object;
- * @private
- */
-bad.MqttParse.prototype.parseCommand_ = function(norm) {
-  var funcName = norm.msg.shift();
-  var fn = this.commandParseFuncs_[funcName] || null;
-  return this.doIt_(fn, norm.msg, bad.MqttParse.replyCode.UNKNOWN_COMMAND);
-};
+    var events = {};
+    var isArrCode = 0;
+    var isNumberCode = 0;
+    goog.array.forEach(msg, function (event) {
+      isArrCode =
+        !this.isArray(event) ? bad.MqttParse.replyCode.EVENT_NOT_ARRAY :
+        this.isEmptyArr(event) ? bad.MqttParse.replyCode.EVENT_ARR_EMPTY :
+        isArrCode;
 
-/**
- * Adds a proxy parsing function for the given command name.
- * This can be called multiple times, and each unique command name will is
- * mapped to its corresponding parsing function.
- * @param {!string} cmdName The name of the command.
- * @param {Function} func The proxy parsing function for the given command.
- */
-bad.MqttParse.prototype.addCommandParseFunc = function(cmdName, func) {
-  this.commandParseFuncs_[cmdName] = func;
-};
+      if (!isArrCode) {
+        var code = event.shift();
+        isNumberCode =
+          !this.isInt(code) ? bad.MqttParse.replyCode.EVENT_CODE_NOT_NUMBER :
+          isNumberCode;
 
-//-------------------------------------------------------------------[ Event ]--
+        if (!isNumberCode) {
+          if (goog.isDef(event[0])) {
+            events[code] = event[0];
+          } else {
+            events[code] = null;
+          }
+        }
+      }
+    }, this);
 
-/**
- * Event parsing function that steps through each of the events as contained in
- * the msg component of the normalised data, and parses each event according to
- * its code. If all events fail, its return code is an outright event fail.
- * If some pass, its code is a partial success code.
- * If all pass, its code is an ALL_OK
- * @param {!bad.MqttParse.NormData} norm The normalised data structure
- * @return {!Array} The reply array;
- * @private
- */
-bad.MqttParse.prototype.parseEvent_ = function(norm) {
-  var arr = [];
-  var failedCodes = [];
-  goog.array.forEach(norm.msg, function(event) {
-    var code = event.shift();
-    var fn = this.eventParseFuncs_[code.toString()];
-    var reply = this.doIt_(fn, event,
-        bad.MqttParse.replyCode.UNKNOWN_ERROR_CODE);
-    if (reply[0] !== 0) {
-      failedCodes.push([code, reply[0], reply[1]]);
-    }
-  }, this);
-
-  if (failedCodes.length) {
-    // Some failed.
-    if (norm.msg.length === failedCodes.length) {
-      // All failed.
-      arr.push(bad.MqttParse.replyCode.UNKNOWN_ERROR_CODE);
+    if (isArrCode) {
+      broken.code = isArrCode;
+      reply = broken;
+    } else if (isNumberCode) {
+      broken.code = isNumberCode;
+      reply = broken;
     } else {
-      arr.push(bad.MqttParse.replyCode.PARTIAL_EVENT_SUCCESS);
-      arr.push(failedCodes);
+      reply.events = events;
     }
   } else {
-    // All passed
-    arr.push(bad.MqttParse.replyCode.ALL_OK);
+    broken.code = reply.code;
+    reply = broken;
   }
-  return arr;
+  return reply;
+};
+
+bad.MqttParse.prototype.parseCommandMsg_ = function(msg, reply) {
+  reply.code =
+    !this.isArray(msg) ? bad.MqttParse.replyCode.COMMAND_NOT_ARRAY :
+    this.isEmptyArr(msg) ? bad.MqttParse.replyCode.COMMAND_ARR_EMPTY :
+    !this.isString(msg[0]) ? bad.MqttParse.replyCode.COMMAND_STRING :
+    reply.code;
+
+  if (!reply.code) {
+    var funcName = msg.shift();
+    reply.command = {};
+    reply.command[funcName] = msg;
+  } else {
+    var broken = {};
+    broken.code = reply.code;
+    reply = broken;
+  }
+
+  return reply;
 };
 
 /**
- * Add a proxy function to parse a specific event code with.
- * This can be called multiple times, and each given code and function is mapped
- * into an map.
- * @param {!number} eventCode The event number.
- * @param {Function} func The parsing function for the given event code.
- */
-bad.MqttParse.prototype.addEventParseFunc = function(eventCode, func) {
-  this.eventParseFuncs_[eventCode.toString()] = func;
-};
-
-//--------------------------------------------------------------------[ Data ]--
-
-/**
- * @param {!bad.MqttParse.NormData} norm The normalised data structure
- * @return {!Array} The reply array;
+ * @param {!string} type
+ * @param {!Array} val
+ * @return {!bad.MqttParse.NormData}
  * @private
  */
-bad.MqttParse.prototype.parseData_ = function(norm) {
-  var fn = this.dataParseFunc_;
-  return this.doIt_(fn, norm.msg, bad.MqttParse.replyCode.DATA_NOT_PARSED);
+bad.MqttParse.prototype.normalizePayload_ = function(type, val) {
+  var reply = {};
+  if (type === 'n') {
+    reply.code = bad.MqttParse.replyCode.ALL_OK;
+    reply.nullMsg = true;
+    reply.ts = new Date();
+  } else {
+    reply = this.testPayloadType_(type, val, reply);
+    if (!reply.code) {
+      reply = this.testPayloadLength_(type, val, reply);
+      if (!reply.code) {
+        reply = this.parseHeader_(type, val, reply);
+      }
+    }
+  }
+  return reply;
 };
 
-/**
- * Add an proxy function to parse data with.
- * @param {Function} func The function itself.
- */
-bad.MqttParse.prototype.setDataParseFunc = function(func) {
-  this.dataParseFunc_ = func;
+bad.MqttParse.prototype.isArray = function(a) {
+    return goog.typeOf(a) === 'array';
 };
 
-//-------------------------------------------------------[ Transaction Reply ]--
-
-/**
- * @param {!bad.MqttParse.NormData} norm The normalised data structure
- * @private
- */
-bad.MqttParse.prototype.parseXReply_ = function(norm) {
-  this.dispatchEvent(new bad.TCTReceivedEvent(this, norm));
+bad.MqttParse.prototype.isNotEmptyArr = function(a) {
+    return !this.isEmptyArr(a);
 };
 
-//------------------------------------------------------------[ Null Message ]--
-
-/**
- * Add an proxy function to a null message with.
- * @param {Function} func The parsing function itself.
- */
-bad.MqttParse.prototype.setNullFunc = function(func) {
-  this.nullFunc_ = func;
+bad.MqttParse.prototype.isEmptyArr = function(a) {
+    return goog.isDef(a[0]) ? false : true;
 };
 
-//------------------------------------------------------------[ Parser Events]--
-
-/**
- * @param {bad.MqttParse} target The parser.
- * @param {!string} tct The MQTT topic on which a reply must be published.
- * @param {!bad.MqttParse.XMsg} payload The payload of the given topic.
- * @constructor
- * @extends {goog.events.Event}
- */
-bad.TCTEvent = function(target, tct, payload) {
-  goog.events.Event.call(this, 'tct', target);
-
-  /**
-   * @type {!string}
-   */
-  this.tct = tct;
-
-  /**
-   * @type {!bad.MqttParse.XMsg}
-   */
-  this.payload = payload;
+bad.MqttParse.prototype.isString = function(a) {
+  return goog.typeOf(a) === 'string';
 };
-goog.inherits(bad.TCTEvent, goog.events.Event);
 
-/**
- * @param {bad.MqttParse} target The parser.
- * @param {!bad.MqttParse.NormData} norm The normalised data.
- * @constructor
- * @extends {goog.events.Event}
- */
-bad.TCTReceivedEvent = function(target, norm) {
-  goog.events.Event.call(this, 'tctreply', target);
-
-  /**
-   * @type {!bad.MqttParse.NormData}
-   */
-  this.tctReply = norm;
+bad.MqttParse.prototype.isNumber = function(a) {
+  return goog.typeOf(a) === 'number';
 };
-goog.inherits(bad.TCTEvent, goog.events.Event);
+
+bad.MqttParse.prototype.isInt = function(a) {
+    var aS = a.toString();
+    return goog.typeOf(a) === 'number' &&
+      a > 0 &&
+      !goog.string.contains(aS, '.');
+};
+
+bad.MqttParse.prototype.isSignedInt = function(a) {
+    var aS = a.toString();
+    return goog.typeOf(a) === 'number' &&
+      !goog.string.contains(aS, '.');
+};
+
+bad.MqttParse.prototype.isObject = function(a) {
+    return goog.typeOf(a) === 'object';
+};
+
+bad.MqttParse.prototype.isDate = function(a) {
+    return Object.prototype.toString.call(a) === '[object Date]';
+};
