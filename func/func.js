186db9f464fd1b6d3ba3a6355ed1718d13c7985a
Jan Badenhorst <jan@trintel.co.za>
2020-10-28 10:54:18 +0200

    More rosettacode things

--- func/func.js
+++ func/func.js
@@ -1899,7 +1899,7 @@ console.log('\n');
     const k = i / 10;
     const v = Math.round(k % 1 * 10);
     const r = Math.floor(k);
-    return r > 0 ? intToArr(r,[v, ...acc]) : [v, ...acc];
+    return r > 0 ? intToArr(r, [v, ...acc]) : [v, ...acc];
   }
 
   console.log(intToArr(95322020));
@@ -1919,13 +1919,13 @@ console.log('\n');
   const minBiggerThanN = (arr, n) => arr.filter(e => e > n).sort()[0];
   const remEl = (arr, e) => {
     const r = arr.indexOf(e);
-    return arr.filter((e,i) => i !== r);
+    return arr.filter((e, i) => i !== r);
   }
 
   const nextHighest = itr => {
     const seen = [];
     let result = 0;
-    for (const [i,v] of itr.entries()) {
+    for (const [i, v] of itr.entries()) {
       const n = +v;
       if (Math.max(n, ...seen) !== n) {
         const right = itr.slice(i + 1);
@@ -1959,4 +1959,282 @@ console.log('\n');
 
 
 })();
+
+console.log('\n\n');
+
+//------------------------------------------------------------------------------
+// Rosetta Code: Determine if a string has all the same characters
+// http://rosettacode.org/wiki/Determine_if_a_string_has_all_the_same_characters
+(() => {
+
+  const check = s => {
+    const arr = [...s];
+    const at = arr.findIndex(
+        (v, i) => i === 0 ? false : v !== arr[i - 1]
+    )
+    const l = arr.length;
+    const ok = at === -1;
+    const p = ok ? "" : at + 1;
+    const v = ok ? "" : arr[at];
+    const vs = v === "" ? v : `"${v}"`
+    const h = ok ? "" : `0x${v.charCodeAt(0).toString(16)}`;
+    console.log(`"${s}" => Length:${l}\tSame:${ok}\tPos:${p}\tChar:${vs}\tHex:${h}`)
+  }
+
+  ['', '   ', '2', '333', '.55', 'tttTTT', '4444 444k',
+    '🐶🐶🐺🐶', '🎄🎄🎄🎄'].forEach(check)
+
+})();
+
+console.log('\n\n');
+
+//------------------------------------------------------------------------------
+// Rosetta Code: Bioinformatics/Sequence mutation
+// http://rosettacode.org/wiki/Bioinformatics/Sequence_mutation
+(() => {
+  // Basic set-up
+  const numBases = 250
+  const numMutations = 30
+  const bases = ['A', 'C', 'G', 'T'];
+
+  // Utility functions
+  /**
+   * Return a shallow copy of an array
+   * @param {Array<*>} arr
+   * @returns {*[]}
+   */
+  const copy = arr => [...arr];
+
+  /**
+   * Get a random int up to but excluding the the given number
+   * @param {number} max
+   * @returns {number}
+   */
+  const randTo = max => (Math.random() * max) | 0;
+
+  /**
+   * Given an array return a random element and the indeg of that element from
+   * the array.
+   * @param {Array<*>} arr
+   * @returns {[*[], number]}
+   */
+  const randSelect = arr => {
+    const at = randTo(arr.length);
+    return [arr[at], at];
+  };
+
+  /**
+   * Given a number or string, return a left padded string
+   * @param {string|number} v
+   * @returns {string}
+   */
+  const pad = v => ('' + v).padStart(4, ' ');
+
+  /**
+   * Count the number of elements that match the given value in an array
+   * @param {Array<string>} arr
+   * @returns {function(string): number}
+   */
+  const filterCount = arr => s => arr.filter(e => e === s).length;
+
+  /**
+   * Utility logging function
+   * @param {string|number} v
+   * @param {string|number} n
+   */
+  const print = (v, n) => console.log(`${pad(v)}:\t${n}`)
+
+  /**
+   * Utility function to randomly select a new base, and an index in the given
+   * sequence.
+   * @param {Array<string>} seq
+   * @param {Array<string>} bases
+   * @returns {[string, string, number]}
+   */
+  const getVars = (seq, bases) => {
+    const [newBase, _] = randSelect(bases);
+    const [extBase, randPos] = randSelect(seq);
+    return [newBase, extBase, randPos];
+  };
+
+  // Bias the operations
+  /**
+   * Given a map of function to ratio, return an array of those functions
+   * appearing ratio number of times in the array.
+   * @param weightMap
+   * @returns {Array<function>}
+   */
+  const weightedOps = weightMap => {
+    return [...weightMap.entries()].reduce((p, [op, weight]) =>
+        [...p, ...(Array(weight).fill(op))], []);
+  };
+
+  // Pretty Print functions
+  const prettyPrint = seq => {
+    let idx = 0;
+    const rem = seq.reduce((p, c) => {
+      const s = p + c;
+      if (s.length === 50) {
+        print(idx, s);
+        idx = idx + 50;
+        return '';
+      }
+      return s;
+    }, '');
+    if (rem !== '') {
+      print(idx, rem);
+    }
+  }
+
+  const printBases = seq => {
+    const filterSeq = filterCount(seq);
+    let tot = 0;
+    [...bases].forEach(e => {
+      const cnt = filterSeq(e);
+      print(e, cnt);
+      tot = tot + cnt;
+    })
+    print('Σ', tot);
+  }
+
+  // Mutation definitions
+  const swap = ([hist, seq]) => {
+    const arr = copy(seq);
+    const [newBase, extBase, randPos] = getVars(arr, bases);
+    arr.splice(randPos, 1, newBase);
+    return [[...hist, `Swapped ${extBase} for ${newBase} at ${randPos}`], arr];
+  };
+
+  const del = ([hist, seq]) => {
+    const arr = copy(seq);
+    const [newBase, extBase, randPos] = getVars(arr, bases);
+    arr.splice(randPos, 1);
+    return [[...hist, `Deleted ${extBase} at ${randPos}`], arr];
+  }
+
+  const insert = ([hist, seq]) => {
+    const arr = copy(seq);
+    const [newBase, extBase, randPos] = getVars(arr, bases);
+    arr.splice(randPos, 0, newBase);
+    return [[...hist, `Inserted ${newBase} at ${randPos}`], arr];
+  }
+
+  // Create the starting sequence
+  const seq = Array(numBases).fill(undefined).map(
+      () => randSelect(bases)[0]);
+
+  // Create a weighted set of mutations
+  const weightMap = new Map()
+      .set(swap, 1)
+      .set(del, 1)
+      .set(insert, 1);
+  const operations = weightedOps(weightMap);
+  const mutations = Array(numMutations).fill(undefined).map(
+      () => randSelect(operations)[0]);
+
+  // Mutate the sequence
+  const [hist, mut] = mutations.reduce((p, c) => c(p), [[], seq]);
+
+  console.log('ORIGINAL SEQUENCE:')
+  prettyPrint(seq);
+
+  console.log('\nBASE COUNTS:')
+  printBases(seq);
+
+  console.log('\nMUTATION LOG:')
+  hist.forEach((e, i) => console.log(`${i}:\t${e}`));
+
+  console.log('\nMUTATED SEQUENCE:')
+  prettyPrint(mut);
+
+  console.log('\nMUTATED BASE COUNTS:')
+  printBases(mut);
+
+})()
+
+console.log('\n\n');
+
+
+//------------------------------------------------------------------------------
+// Rosetta Code: Bioinformatics/base count
+// http://rosettacode.org/wiki/Bioinformatics/base_count
+(() => {
+  const rowLength = 50;
+
+  /**
+   * Convert the given array into an array of smaller arrays each with the length
+   * given by n.
+   * @param {number} n
+   * @returns {function(!Array<*>): !Array<!Array<*>>}
+   */
+  const chunk = n => a => a.reduce(
+      (p, c, i) => (!(i % n)) ? p.push([c]) && p : p[p.length - 1].push(c) && p,
+      []);
+  const toRows = chunk(rowLength);
+
+  /**
+   * Given a number, return function that takes a string and left pads it to n
+   * @param {number} n
+   * @returns {function(string): string}
+   */
+  const padTo = n => v => ('' + v).padStart(n, ' ');
+  const pad = padTo(5);
+
+  /**
+   * Count the number of elements that match the given value in an array
+   * @param {Array<string>} arr
+   * @returns {function(string): number}
+   */
+  const countIn = arr => s => arr.filter(e => e === s).length;
+
+  /**
+   * Utility logging function
+   * @param {string|number} v
+   * @param {string|number} n
+   */
+  const print = (v, n) => console.log(`${pad(v)}:\t${n}`)
+
+
+// Pretty Print functions
+  const prettyPrint = seq => {
+    const chunks = toRows(seq);
+    chunks.forEach((e, i) => print(i * rowLength, e.join('')))
+  }
+
+  const printBases = (seq, bases) => {
+    const filterSeq = countIn(seq);
+    const counts = bases.map(filterSeq);
+    const total = counts.reduce((p,c) => p + c, 0);
+    counts.forEach((e, i) => print(bases[i], e));
+    print('Total', total);
+  }
+
+  const bases = ['A', 'C', 'G', 'T'];
+
+// Create the starting sequence
+  const seq = `CGTAAAAAATTACAACGTCCTTTGGCTATCTCTTAAACTCCTGCTAAATG
+CTCGTGCTTTCCAATTATGTAAGCGTTCCGAGACGGGGTGGTCGATTCTG
+AGGACAAAGGTCAAGATGGAGCGCATCGAACGCAATAAGGATCATTTGAT
+GGGACGTTTCGTCGACAAAGTCTTGTTTCGAGAGTAACGGCTACCGTCTT
+CGATTCTGCTTATAACACTATGTTCTTATGAAATGGATGTTCTGAGTTGG
+TCAGTCCCAATGTGCGGGGTTTCTTTTAGTACGTCGGGAGTGGTATTATA
+TTTAATTTTTCTATATAGCGATCTGTATTTAAGCAATTCATTTAGGTTAT
+CGCCGCGATGCTCGGTTCGGACCGCCAAGCATCTGGCTCCACTGCTAGTG
+TCCTAAATTTGAATGGCAAACACAAATAAGATTTAGCAATTCGTGTAGAC
+GACCGGGGACTTGCATGATGGGAGCAGCTTTGTTAAACTACGAACGTAAT`
+      .split('')
+      .filter(e => bases.includes(e))
+
+  console.log('SEQUENCE:')
+  prettyPrint(seq);
+
+  console.log('\nBASE COUNTS:')
+  printBases(seq, bases);
+
+})()
+
+
+
+
+
 
